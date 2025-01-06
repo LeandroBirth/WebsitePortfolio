48da720c6211438f4f0edb7ee03c9a88a3de4697
Leandro Birth (Nascimento) <152208959+LeandroBirth@users.noreply.github.com>
2025-01-06 15:12:16 -0300

    Add files via upload

---
+++ Code/mobileMenu.js
@@ -0,0 +1,24 @@
+console.log('mobile menu is running');
+
+const mobileMenuIcon = document.getElementById('mobileMenuIcon');
+const mobileMenuContainer = document.getElementById('mobileMenuContainer');
+const mobileMenu = document.getElementById('mobileMenu');
+const mobileMenuLink = document.querySelectorAll('.mobile-menu__link');
+
+mobileMenuIcon.addEventListener('click', handleToggle);
+mobileMenuLink.forEach(element => {
+    element.addEventListener('click', handleClose);
+});
+
+function handleToggle () {
+    console.log('icon clicked');
+    mobileMenuContainer.classList.toggle('menu-open');
+    setTimeout(() => mobileMenu.classList.toggle('menu-open'), 10);
+    mobileMenuIcon.classList.toggle('menu-open');
+}
+
+function handleClose () {
+    mobileMenuContainer.classList.contains('menu-open') ? mobileMenuContainer.classList.remove('menu-open') : null;
+    mobileMenuIcon.classList.contains('menu-open') ? mobileMenuIcon.classList.remove('menu-open'): null;
+    mobileMenu.classList.contains('menu-open') ? mobileMenu.classList.remove('menu-open'): null;
+}
